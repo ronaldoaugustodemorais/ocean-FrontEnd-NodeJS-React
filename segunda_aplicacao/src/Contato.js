a489df76665dafcc2d3400b343609a068d1f2049
RonaldoAugusto <ronaldoaugustodemorais@gmail.com>
2021-08-26 12:47:00 -0300

    Criado as paginas: Inicial, Contato. Inicio da Implementacao do Router>index.js

---
+++ segunda_aplicacao/src/Contato.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Contato extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Esta é a página de Contato !</p>
+      </div>
+    );
+  }
+}
+
+export default Contato;
--- segunda_aplicacao/src/index.js
+++ segunda_aplicacao/src/index.js
@@ -2,8 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Route from "react-router-dom";
 import Agua from "./Agua";
+import Inicial from "./Inicial";
+import Contato from "./Contato";
+
+import { Route, NavLink, HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <Agua fusao={0} ebulicao={100} condicao="CNTP" />,
+  /*  <Agua fusao={0} ebulicao={100} condicao="CNTP" />,
+  document.getElementById("root")
+*/
+  <HashRouter>
+    <div>
+      <h1>Single Page Application - SPA</h1>
+      <ul>
+        <li>
+          <NavLink href="/inicial">Inicial</NavLink>
+        </li>
+        <li>
+          <NavLink href="/contato">Contato</NavLink>
+        </li>
+      </ul>
+    </div>
+  </HashRouter>,
   document.getElementById("root")
 );
