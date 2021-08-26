d6463acb79ab9fb4f9a2d3f4e8e361235529511e
RonaldoAugusto <ronaldoaugustodemorais@gmail.com>
2021-08-26 10:15:52 -0300

    Criado funcoes e lista. Modificacao do CSS

--- primeira_aplicacao/src/index.js
+++ primeira_aplicacao/src/index.js
@@ -1,8 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
-function Inicial() {
-  return <p>Testando o Titulo da App!</p>;
+const lista = [
+  {
+    id: 10,
+    nome: "Antonio",
+  },
+  {
+    id: 20,
+    nome: "Deusany",
+  },
+  {
+    id: 30,
+    nome: "de Carvalho",
+  },
+  {
+    id: 40,
+    nome: "Junior",
+  },
+];
+
+function Pessoas() {
+  return (
+    <div className="pessoas">
+      {lista.map((item, index) => (
+        <p>
+          {index} : {item.nome}
+        </p>
+      ))}
+    </div>
+  );
 }
 
-ReactDOM.render(<Inicial />, document.getElementById("root"));
+function Pessoa(props) {
+  return (
+    <div className="pessoa">
+      id={lista[props.id - 1].id} : nome={lista[props.id - 1].nome}
+    </div>
+  );
+}
+
+ReactDOM.render(<Pessoas />, document.getElementById("root"));
